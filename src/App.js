4080d4badf784003a1bea95c3200e0ab4036ab54
Glenn Ludszuweit <glenn.ludszuweit@gmail.com>
2021-02-06 22:23:57 +0100

    make code better

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ function App() {
   const [selectedEmotion, setSelectedEmotion] = useState(['c3']);
   const [play, setPlay] = useState(true);
 
-  for (let i = 0; i < winningCombo.length; i++) {
+  winningCombo.forEach((combo) => {
     let exists = 0;
-    for (let j = 0; j < 5; j++) {
-      if (selectedEmotion.indexOf(winningCombo[i][j]) > -1) {
+    combo.forEach((el) => {
+      if (selectedEmotion.indexOf(el) > -1) {
         exists++;
       }
-    }
-    if (exists === 5) {
-      setTimeout(() => {
-        setPlay(false);
-      }, 1000);
-    }
-  }
+      if (exists === 5) {
+        setTimeout(() => {
+          setPlay(false);
+        }, 1000);
+      }
+    });
+  });
 
   return (
     <Main>
